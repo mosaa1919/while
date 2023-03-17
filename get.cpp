ee341586e8bdc04d748f879ef7c66761d20032ec
mosaa1919 <128163894+mosaa1919@users.noreply.github.com>
2023-03-17 17:21:48 +0200

    Add files via upload

---
+++ get.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+
+
+int x = 0;
+
+while (x < 4)
+{
+    cout << x << endl;
+    
+}
+
+
+
+
+
+    return 0;
+
+    
+} // namespace std;
+
